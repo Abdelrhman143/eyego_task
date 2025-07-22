5b8de6050cd50f08c8ee338136a0d698e2ed5b87
Abdelrhman143 <aa632481@gmail.com>
2025-07-22 11:45:59 +0300

    finsh ui of the header

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -1,7 +1,19 @@
+import logo from "@/public/logo.jpeg";
+import { LogOut } from "lucide-react";
+import Image from "next/image";
+
 export default function Header() {
   return (
-    <header className="bg-gray-400">
-      <div className="container">Header</div>
+    <header className="py-2 shadow-sm shadow-gray-300 mb-4 flex items-center">
+      <div className="container flex items-center gap-2">
+        <Image src={logo} alt="logo" className="w-10 h-10 rounded" />
+        <h1 className="font-bold">EyeGo Ai</h1>
+      </div>
+      <div className="mr-3">
+        <button>
+          <LogOut />
+        </button>
+      </div>
     </header>
   );
 }
